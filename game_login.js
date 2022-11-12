ec13c481ae100ed8e9ae4b1b0d377d3fb9511326
TathaiSardar01 <91777520+TathaiSardar01@users.noreply.github.com>
2022-11-12 15:23:10 +0530

    Delete game_login.js

--- game_login.js
+++
@@ -1,9 +0,0 @@
-function addUser() {
-    player_1_name = document.getElementById("player1_name_input").value;
-    player_2_name = document.getElementById("player2_name_input").value;
-
-    localStorage.setItem("player_1_name", player_1_name);
-    localStorage.setItem("player_2_name", player_2_name);
-
-    window.location="game_page.html";
-}
